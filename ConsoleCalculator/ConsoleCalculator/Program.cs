0c984083fce9aa01e5e619cf0f8ba5e5a1a46c79
yeongjuBae <wnqo925@gmail.com>
2022-11-08 22:51:53 +0900

    테스트 겸 간단하게 입력 출력 추가

--- ConsoleCalculator/ConsoleCalculator/Program.cs
+++ ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -6,7 +6,24 @@ namespace ConsoleCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string stringNumber;
+            int number= 0;
+            bool isNumber = false;
+
+            Console.WriteLine("[Calculater]");
+            Console.Write("입력:");
+            stringNumber = Console.ReadLine();
+
+            //number = Convert.ToInt32(stringNumber);
+
+            if(int.TryParse(stringNumber, out number))
+            {
+                Console.WriteLine("결과:" + number);
+            }
+            else
+            {
+                Console.WriteLine("결과: 문자열이 들어갔음");
+            }
         }
     }
 }
