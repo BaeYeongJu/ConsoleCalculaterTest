973b84c4d6d29590340224a52c30007711426332
yeongjuBae <wnqo925@gmail.com>
2022-11-14 21:34:48 +0900

    테스트 코드 쪼개기

--- ConsoleCalculator/TestProject1/UnitTest1.cs
+++ ConsoleCalculator/TestProject1/UnitTest1.cs
@@ -12,11 +12,53 @@ namespace TestProject1
 
         [TestMethod]
         [DataRow(0, 10)]
-        public void TestMethod1(int a,int b)
+        public void CalculateAddTest(int a,int b)
         {
             var main = new Program();
             //main.Add(a, b) == a+b
-            Assert.AreEqual(main.Add(a, b), a+b);
+
+            Assert.AreEqual(main.Add(a, b), a + b);
+        }
+
+        [TestMethod]
+        [DataRow(20, 10)]
+        public void CalculateSubTest(int a, int b)
+        {
+            var main = new Program();
+            //main.Sub(a, b) == a-b
+
+            Assert.AreEqual(main.Sub(a, b), a - b);
+        }
+
+        [TestMethod]
+        [DataRow(-3, 10)]
+        public void CalculateMultiplicationTest(int a, int b)
+        {
+            var main = new Program();
+
+            //main.Multiplication(a, b) == a*b
+            Assert.AreEqual(main.Multiplication(a, b), a * b);
+        }
+
+        [TestMethod]
+        [DataRow(14, 10)]
+        public void CalculateDivisionTest(int a, int b)
+        {
+            var main = new Program();
+
+            //main.Multiplication(a, b) == a/b
+            Assert.AreEqual(main.Division(a, b), a / b);
+            Assert.AreEqual(main.Remainder(a, b), a % b);
+        }
+
+        [TestMethod]
+        [DataRow(3, 2)]
+        public void CalculateRemainderTest(int a, int b)
+        {
+            var main = new Program();
+
+            //main.Remainder(a, b) == a%b
+            Assert.AreEqual(main.Remainder(a, b), a % b);
         }
     }
 }
