53e5b20c8b64818ae0fcd98697574437a44a7630
yeongjuBae <wnqo925@gmail.com>
2022-11-17 19:06:51 +0900

    int형 변수들 double형으로 변경

--- ConsoleCalculator/ConsoleCalculator/Program.cs
+++ ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -10,14 +10,14 @@ namespace ConsoleCalculator
             CalculateManager calculateManager = new CalculateManager();
 
             Console.WriteLine("[Calculater]");
-            Console.Write("첫번째 숫자 입력 (정수):");
+            Console.Write("첫번째 숫자 입력 :");
 
             string firstStringNumber = string.Empty;
             string secondStringNumber = string.Empty;
             string operatorSymbol = string.Empty;
 
-            int firstNumber = 0;
-            int secondNumber = 0;
+            double firstNumber = 0f;
+            double secondNumber = 0f;
 
             firstStringNumber = Console.ReadLine();
             calculateManager.ShowErrorMessage(firstStringNumber);
@@ -25,13 +25,13 @@ namespace ConsoleCalculator
             Console.Write("연산자 입력 (+,-,*,/,%):");
             operatorSymbol = Console.ReadLine();
 
-            Console.Write("두번째 숫자 입력 (정수):");
+            Console.Write("두번째 숫자 입력 :");
             secondStringNumber = Console.ReadLine();
 
             calculateManager.ShowErrorMessage(secondStringNumber);
 
-            var result = calculateManager.Calculate(int.Parse(firstStringNumber), int.Parse(secondStringNumber), operatorSymbol);
-            Console.WriteLine($"결과: {int.Parse(firstStringNumber) + operatorSymbol + int.Parse(secondStringNumber)} = {result} ");
+            var result = calculateManager.Calculate(double.Parse(firstStringNumber), double.Parse(secondStringNumber), operatorSymbol);
+            Console.WriteLine($"결과: {double.Parse(firstStringNumber) + operatorSymbol + double.Parse(secondStringNumber)} = {result} ");
         }
     }
 }
