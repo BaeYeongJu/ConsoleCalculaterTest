d312f28e0713fcab05d3b157762a498666f9530a
yeongjuBae <yjbae@virnect.com>
2022-11-22 14:06:44 +0900

    결과 출력하는 메서드 수정 (window 계산기와 유사하게 출력)

--- ConsoleCalculator/ConsoleCalculator/UIManager.cs
+++ ConsoleCalculator/ConsoleCalculator/UIManager.cs
@@ -6,21 +6,23 @@ namespace ConsoleCalculator
 {
     public class UIManager
     {
-
-        private void ShowMessage(string firstStringNumber, string operatorSymbol, double result)
+        private void ShowMessage(string firstStringNumber, double result)
         {
-            Console.WriteLine($"결과: {double.Parse(firstStringNumber) + operatorSymbol} = {result} ");
+            Console.WriteLine($"결과: 1/({double.Parse(firstStringNumber)}) = {result} ");
         }
 
         private void ShowMessage(string firstStringNumber, string operatorSymbol, string secondStringNumber, double result)
         {
-            Console.WriteLine($"결과: {double.Parse(firstStringNumber) + operatorSymbol + double.Parse(secondStringNumber)} = {result} ");
+            if (operatorSymbol == "." || operatorSymbol == "n")
+                Console.WriteLine($"결과: {result} ");
+            else
+                Console.WriteLine($"결과: {double.Parse(firstStringNumber) + operatorSymbol + double.Parse(secondStringNumber)} = {result} ");
         }
 
         public void ShowResult(CalculateManager calculateManager, string firstStringNumber, string operatorSymbol, double result)
         {
             result = calculateManager.Calculate(double.Parse(firstStringNumber), operatorSymbol);
-            ShowMessage(firstStringNumber, operatorSymbol, result);
+            ShowMessage(firstStringNumber, result);
         }
 
         public void ShowResult(CalculateManager calculateManager, string firstStringNumber, string secondStringNumber, string operatorSymbol, double result)
